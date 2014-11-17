5571bb4f36e312e9d8549b6cca82cd73d25ddd77
Reid Weber <reid_weber@hotmail.com>
2014-11-17 10:56:00 -0800

    Subclassed ViewControllers

---
+++ SwapMeet/SMProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  SMProfileViewController.h
+//  SwapMeet
+//
+//  Created by Reid Weber on 11/17/14.
+//  Copyright (c) 2014 SwapMeet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SMProfileViewController : UIViewController
+
+@end
---
+++ SwapMeet/SMProfileViewController.m
@@ -0,0 +1,37 @@
+//
+//  SMProfileViewController.m
+//  SwapMeet
+//
+//  Created by Reid Weber on 11/17/14.
+//  Copyright (c) 2014 SwapMeet. All rights reserved.
+//
+
+#import "SMProfileViewController.h"
+
+@interface SMProfileViewController ()
+
+@end
+
+@implementation SMProfileViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
