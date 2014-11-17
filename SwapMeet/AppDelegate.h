6f5ac9abd59bb0a198ea4c1a92704d3a99c8c6dd
Kevin Pham <kevin.v.pham@hotmail.com>
2014-11-17 10:31:06 -0800

    initial commit

---
+++ SwapMeet/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  SwapMeet
+//
+//  Created by Kevin Pham on 11/17/14.
+//  Copyright (c) 2014 SwapMeet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ SwapMeet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SwapMeet
+//
+//  Created by Kevin Pham on 11/17/14.
+//  Copyright (c) 2014 SwapMeet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
