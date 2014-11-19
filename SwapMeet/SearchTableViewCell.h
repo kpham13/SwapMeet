3e6746d676bcf30c7c475dc9172de3860732a1ca
Reid Weber <reid_weber@hotmail.com>
2014-11-19 10:31:16 -0800

    Created Custom Cell - Hooked Up Search To Network Controller

---
+++ SwapMeet/SearchTableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  SearchTableViewCell.h
+//  SwapMeet
+//
+//  Created by Reid Weber on 11/19/14.
+//  Copyright (c) 2014 SwapMeet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+
+
+@end
