fbe876f115fd1c22df5feda4c7772b4547696a2c
Reid Weber <reid_weber@hotmail.com>
2014-11-20 13:08:06 -0800

    Finished Table Cell Design

---
+++ SwapMeet/MatchTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MatchTableViewCell.h
+//  SwapMeet
+//
+//  Created by Reid Weber on 11/20/14.
+//  Copyright (c) 2014 SwapMeet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MatchTableViewCell : UITableViewCell
+
+@end
---
+++ SwapMeet/MatchTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  MatchTableViewCell.m
+//  SwapMeet
+//
+//  Created by Reid Weber on 11/20/14.
+//  Copyright (c) 2014 SwapMeet. All rights reserved.
+//
+
+#import "MatchTableViewCell.h"
+
+@implementation MatchTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
